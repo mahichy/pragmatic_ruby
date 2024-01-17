4c87d0fb9c591f5cea3d2dc717e4ff14dbb65710
mahichy <mohiuddinoshim@yahoo.com>
2024-01-17 22:47:59 +0600

    ABCs and 123s

---
+++ 02/flicks.rb
@@ -0,0 +1,6 @@
+title = "Ghostbusters"
+rank = 10
+name = "Mikey"
+
+puts "#{title} has a rank of #{rank}."
+puts "#{name}/s favorite movie is #{title}. \nHe gives it a #{rank}!"
---
+++ 02/studio_game.rb
@@ -0,0 +1,7 @@
+puts "Let's play a game!\n\n\t3\n\t2\n\t1"
+
+name = "Finn"
+health = 60
+puts "#{name}'s health is #{health * 3} "
+puts "#{name}'s health is #{health / 9} "
+puts "#{name}'s health is #{health / 9.0} "
